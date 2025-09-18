b9f755c0bddff4acaa1a0ef854f20b980d379a66
Swastika Yadav <Swastikayadav5358@gmail.com>
2025-09-18 15:33:01 +0530

    query matching implemented

--- cli_assistant/ai_engine.py
+++ cli_assistant/ai_engine.py
@@ -1,6 +1,7 @@
 import yaml
 from sentence_transformers import SentenceTransformer
 import numpy as np
+from numpy.linalg import norm
 from pathlib import Path
 
 def load_commands(yaml_path: str = "cli_assistant/commands.yml"):
@@ -48,8 +49,34 @@ def save_index(index, file_path="cli_assistant/command_index.npy"):
 def load_index(file_path="cli_assistant/command_index.npy"):
     return np.load(file_path)
 
+
+#quering pipeline
+
+def find_best_match(user_input, index, top_k):
+    
+    query_vec = model.encode(user_input)
+    
+    #comparing with all vectors in index
+    scores = []
+    for entry in index:
+        score = np.dot(query_vec, entry["vector"]) / (norm(query_vec)* (norm(entry["vector"])))
+        scores.append((entry, score))
+        
+    scores = sorted(scores, key=lambda x: x[1], reverse=True)
+    
+    return scores[:top_k]
+
+
 if __name__ == "__main__":
     cmds = load_commands()
     index = build_index(cmds)
     print(f"built index with {len(index)} commands.")
     print("example entry:", index[0]["id"], "->", index[0]["vector"][:5], ["..."]) 
+    
+    #sample query
+    user_query = "stop the currently running process"
+    matches = find_best_match(user_query, index, top_k=3)
+    
+    print(f"\nQuery: {user_query}")
+    for entry, score in matches:
+        print(f"\nmatch: {entry['id']} | score: {score:.3f} | template: {entry['command_template']}") 
